aba129f975603bcb3029aa65b682b9d5c2a64e02
Rob <ricowilliams@protonmail.com>
2022-04-24 11:35:05 -0600

    percent encoded mongouri so connection would work, set up npm run dev

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+const mongoUri =
+  'mongodb+srv://admin:7RPR2%40Ht%40gvbb8g@cluster0.vt6lw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+//above is percentencoded
+// 'mongodb+srv://admin:7RPR2@Ht@gvbb8g@cluster0.vt6lw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+mongoose.connect(mongoUri);
+
+mongoose.connection.on('connected', () => {
+  console.log('connected to Mongo Instance');
+});
+mongoose.connection.on('error', (err) => {
+  console.error('Error connectiong to mongo', err);
+});
 
 app.get('/', (req, res) => {
   res.send('Hi There! You made it');
